86b1ecec281c71db0fec79a5cc77ddced1074fab
Andy <andy.smithwick@gmail.com>
2020-11-12 09:58:07 -0800

    Added friendship wall script, got Java working.
    Project working, my problem was that I opened the wrong folder, should have opened the middle folder FtcRobotControllerUltimateGoal instead of the level above it

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheFriendshipWall.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
+@Disabled
+public class TheFriendshipWall {
+    DcMotor motor1;
+
+}
