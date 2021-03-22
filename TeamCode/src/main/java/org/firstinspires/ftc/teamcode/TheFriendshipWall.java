a5083cfd59a4efa1e523a2ab42f0d21c5cd75c28
Andy Smithwick <andy.smithwick@gmail.com>
2021-03-21 18:04:53 -0700

    Cleaned Up TeleOp_Abstract to match new variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheFriendshipWall.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TheFriendshipWall.java
@@ -1,13 +1,27 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@TeleOp(name="Basic: Iterative OpMode", group="Iterative Opmode")
-@Disabled
-public class TheFriendshipWall {
+
+@TeleOp(name="Friendship Wall", group="Iterative Opmode")
+
+public class TheFriendshipWall extends TeleOp_Abstract {
     DcMotor motor1;
 
-        //Andy Smithwick
+    @Override
+    public void init() {
+        initStuff();
+    }
+
+    @Override
+    public void loop() {
+        rb.setPower(gamepad1.right_stick_y);
+        telemetry.addData("Rb Power", rb.getPower());
+        telemetry.update();
+    }
+
+    //Andy Smithwick
 }
